5b0f3c5c70e3722352c46bfd7e4abfceaf8382e7
Clara <nneamakaclara13@gmail.com>
2024-06-17 07:34:33 +0200

    modified env, index, package json, test, delete deployment.yml, and create new file cicd.yml

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 
 const db = await mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '3306',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
     //database: process.env.DB_NAME || 'booklibrary_test'
