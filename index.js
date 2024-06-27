59c56e8c3862a61f3e44bc935ed3e6e9037256d4
Clara <nneamakaclara13@gmail.com>
2024-06-27 12:04:21 +0200

    modified env, workflow, index, and test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const db = await mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || '3306',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
