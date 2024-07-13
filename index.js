fbddc1a4517894559aeb8a1e5a67bed69a4676fb
Clara <nneamakaclara13@gmail.com>
2024-07-13 10:38:05 +0200

    Modifying all

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let db;
 
 const initializeDbConnection = async () => {
     db = await mysql.createConnection({
-        host: process.env.DB_HOST || 'db',
+        host: process.env.DB_HOST || '127.0.0.1',
         port: process.env.DB_PORT || '3306',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || 'root',
